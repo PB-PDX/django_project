9be4611cdf27b959784db1950da23b806a22d80f
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-09-01 19:14:07 -0700

    updates

--- users/models.py
+++ users/models.py
@@ -10,9 +10,11 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 
+    def save(self):
+        super().save()
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
