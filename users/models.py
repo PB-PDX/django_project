8dfbe54c14393d9771e8ef31cbfc9e2517e5e723
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-09-01 19:17:30 -0700

    updates

--- users/models.py
+++ users/models.py
@@ -4,17 +4,16 @@ from PIL import Image
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    # def save(self, *args, **kwargs):
-    #     super().save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
-    def save(self):
-        super().save()
+    
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
