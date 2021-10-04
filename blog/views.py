1d956d172fe1aac3d7749b655f5163efb3b44026
PBHIPTOP <83720399+PBHIPTOP@users.noreply.github.com>
2021-10-04 06:54:48 -0700

    search results newest

--- blog/views.py
+++ blog/views.py
@@ -27,7 +27,7 @@ class SearchResultsView(ListView):
         query = self.request.GET.get('q')
 
         object_list = Post.objects.filter(
-            Q(title__icontains=query) | Q(content__icontains=query)
+            Q(title__icontains=query) | Q(content__icontains=query) | '-created_date'
         )
         return object_list
 
